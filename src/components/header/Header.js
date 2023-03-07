4c334f6d34f8b06ca667bd0a59e15a466bf250b2
Jake Felice <jasonfelice0@gmail.com>
2023-03-07 02:45:04 -0800

    Create header layout

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,7 +3,7 @@ import Style from './header.module.scss';
 const Header = () => (
   <header className={Style.app__header}>
     <a href="/#">Jake Felice</a>
-    <nav>
+    <nav className={Style.app__headerNav}>
       <ul>
         <li><a href="#about">About</a></li>
         <li><a href="#portfolio">Portfolio</a></li>
