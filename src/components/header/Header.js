edc85fbe3a1ec381faf8f2f9fd23a803f3ff8b61
Jake Felice <jasonfelice0@gmail.com>
2023-03-07 01:48:38 -0800

    Create a header component with nav items

---
+++ src/components/header/Header.js
@@ -0,0 +1,15 @@
+const Header = () => (
+  <header>
+    <a href="/#">Jake Felice</a>
+    <nav>
+      <ul>
+        <li><a href="#about">About</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#portfolio">Contact</a></li>
+      </ul>
+    </nav>
+    <span>Email</span>
+  </header>
+);
+
+export default Header;
