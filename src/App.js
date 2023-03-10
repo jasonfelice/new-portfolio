65b24bf7cd8c9908413e7d7b8e33bd7a03adacca
Jake Felice <jasonfelice0@gmail.com>
2023-03-10 07:08:05 -0800

    Set up projects section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import About from './components/about/About';
 import Header from './components/header/Header';
 import Main from './components/main/Main';
+import Projects from './components/projects/Projects';
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <Main />
       <About />
+      <Projects />
     </div>
   );
 }
---
+++ src/components/projects/Projects.js
@@ -0,0 +1,11 @@
+import Style from './projects.module.scss';
+
+const Projects = () => (
+  <section className={Style.projects}>
+    <div>
+      Projects
+    </div>
+  </section>
+);
+
+export default Projects;
