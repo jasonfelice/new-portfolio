41b793ed8a1a9938b2bb10c18ef9363edd5300cf
Jake Felice <jasonfelice0@gmail.com>
2023-03-19 04:54:28 -0700

    Set up headline in portfolio section

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -2,8 +2,10 @@ import Style from './projects.module.scss';
 
 const Projects = () => (
   <section className={Style.projects}>
-    <div>
-      Projects
+    <div className={Style.headline}>
+      <div className={Style.headlineBar} />
+      <span>Portfolio</span>
+      <div className={Style.headlineBar} />
     </div>
   </section>
 );
