df0a5509f7c9eeec8c31a5eda9c55a382717dda4
Jake Felice <jasonfelice0@gmail.com>
2023-03-20 03:49:44 -0700

    Layout elements of contact form

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,3 +1,4 @@
+import TextField from '@mui/material/TextField';
 import Style from './contact.module.scss';
 
 const Contact = () => (
@@ -6,6 +7,28 @@ const Contact = () => (
       <div className={Style.headlineBar} />
       <span>Connect with me</span>
       <div className={Style.headlineBar} />
+    </div>
+    <div className={Style.contactWrapper}>
+      <div className={Style.form}>
+        <p>
+          I&apos;m always interested in hearing about new projects,
+          so feel free to reach out if you&apos;d like to get in touch.
+        </p>
+        <form>
+          <TextField id="outlined-basic" label="Name" variant="outlined" />
+          <TextField id="outlined-basic" type="email" label="Email" variant="outlined" />
+          <TextField
+            id="outlined-multiline-flexible"
+            label="Multiline"
+            multiline
+            rows={4}
+            defaultValue="Default Value"
+          />
+          <div className={Style.formPanel}>
+            <input type="submit" value="Submit" />
+          </div>
+        </form>
+      </div>
     </div>
   </section>
 );
