aba73f76620442a198ff48781ecad2420ce8dd09
Jake Felice <jasonfelice0@gmail.com>
2023-03-20 08:38:43 -0700

    Layout the footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,8 +1,43 @@
+/* eslint-disable no-unused-vars */
 import Style from './footer.module.scss';
+import github from '../../assets/social-icons/github.svg';
+import linkedin from '../../assets/social-icons/linkedin.svg';
+import twitter from '../../assets/social-icons/twitter.svg';
 
 const Footer = () => (
   <footer className={Style.footer}>
-    <span>Jake Felice 2023</span>
+    <div className={Style.backTop}>
+      <span>Back to top</span>
+      <i />
+    </div>
+    <span>
+      © Jake Felice
+      {' '}
+      { new Date().getFullYear()}
+    </span>
+    <div className={Style.contactList}>
+      <h3>Connect</h3>
+      <ul className={Style.social}>
+        <li>
+          <a target="_blank" href="/#">
+            <i style={{ backgroundImage: `url(${linkedin})` }} />
+            <span>LinkedIn</span>
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="/#">
+            <i style={{ backgroundImage: `url(${github})` }} />
+            <span>GitHub</span>
+          </a>
+        </li>
+        <li>
+          <a target="_blank" href="/#">
+            <i style={{ backgroundImage: `url(${twitter})` }} />
+            <span>Twitter</span>
+          </a>
+        </li>
+      </ul>
+    </div>
   </footer>
 );
 
