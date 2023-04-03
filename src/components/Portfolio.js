b73b25bc41c5f77c84da3051cd6563aa2537bdcb
Jake Felice <jasonfelice0@gmail.com>
2023-04-03 01:26:47 -0700

    Set up carousel layout

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,9 +1,28 @@
+import { motion } from 'framer-motion';
+import { useRef, useEffect, useState } from 'react';
 import Styles from './portfolio.module.scss';
 
-const Portfolio = () => (
-  <div className={Styles.wrapper}>
-    Slider goes here
-  </div>
-);
+const temp = [1, 2, 3, 4, 5];
+
+const Portfolio = () => {
+  const [width, setWidth] = useState(0);
+  const carousel = useRef();
+
+  useEffect(() => {
+    setWidth(carousel.current.scrollWidth - carousel.current.offsetWidth);
+  }, []);
+
+  return (
+    <motion.div ref={carousel} className={Styles.carousel}>
+      <motion.div drag="x" dragConstraints={{ right: 0, left: -width }} className={Styles.innerCarousel}>
+        {
+        temp.map((x) => (
+          <motion.div key={x} className={Styles.box} />
+        ))
+      }
+      </motion.div>
+    </motion.div>
+  );
+};
 
 export default Portfolio;
