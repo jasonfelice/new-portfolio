6102f09979f77c321c3a1aef15b8d19ad552a5bf
Jake Felice <jasonfelice0@gmail.com>
2023-08-07 00:56:13 -0700

    Create a cursor component

---
+++ src/components/Cursor/Cursor.js
@@ -0,0 +1,18 @@
+import Style from './cursor.module.scss';
+
+const Cursor = () => (
+  <>
+    <div style={Style.circle} />
+    <div style={Style.circle} />
+    <div style={Style.circle} />
+    <div style={Style.circle} />
+    <div style={Style.circle} />
+    <div style={Style.circle} />
+    <div style={Style.circle} />
+    <div style={Style.circle} />
+    <div style={Style.circle} />
+    <div style={Style.circle} />
+  </>
+);
+
+export default Cursor;
