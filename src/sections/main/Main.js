20552f6bc1fe1d4cf6e5eee4586831daacdb2085
Jake Felice <jasonfelice0@gmail.com>
2023-08-23 01:43:59 -0700

    Update name

--- src/sections/main/Main.js
+++ src/sections/main/Main.js
@@ -2,7 +2,11 @@ import Style from './main.module.scss';
 
 const Main = () => (
   <section className={Style.main}>
-    <h1 className={Style.title}>Hi, I&apos;m Jake Felice</h1>
+    <h1 className={Style.title}>
+      Hi, I&apos;m Shivam
+      <br />
+      <span>aka Jake</span>
+    </h1>
   </section>
 );
 
